9548f8d3935d683a80709b8e04c14dd4f28a0801
thegreatcode1 <suhaibpa72@gmail.com>
2025-10-25 17:09:30 +0530

    first step ui building

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Banner.css"
+
+function Banner() {
+    return (
+        <div className='banner' >
+            <div className="content">
+                <h1 className='tilte' >Movie Name</h1>
+                <div >
+                    <button className='button' > Play </button>
+                    <button className='button' > My List </button>
+                </div>
+                <h1 className="description">When the national mint and a touring school group are held hostage by robbers, police believe that the</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Banner
